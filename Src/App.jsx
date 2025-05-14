5692caa8af30c4d688c79b469c69a04523b654aa
En-mohammed1234 <enig780@gmail.com>
2025-05-14 18:12:37 +0300

    Create App.jsx

---
+++ Src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LocalStorageForm from './local_storage'; // هذا هو سطر الاستيراد
+
+function App() {
+  return (
+    <div>
+      <LocalStorageForm />
+    </div>
+  );
+}
+
+export default App;
